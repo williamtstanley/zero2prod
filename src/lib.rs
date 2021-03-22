d1108f9b2bce3a06b7b691bb2d1287c8acb48eb2
Travis Stanley <travis@aetherx.io>
2021-03-21 21:41:21 -0700

    Set versions to the working state after the false positive and [0m[0m[1m[32m    Fetching[0m advisory database from `https://github.com/RustSec/advisory-db.git` [0m[0m[1m[32m      Loaded[0m 263 security advisories (from /home/travis/.cargo/advisory-db) [0m[0m[1m[32m    Updating[0m crates.io index [0m[0m[1m[32m    Scanning[0m Cargo.lock for vulnerabilities (164 crate dependencies) [0m[0m[1m[31mCrate:        [0m actix-codec [0m[0m[1m[31mVersion:      [0m 0.4.0-beta.1 [0m[0m[1m[31mTitle:        [0m Use-after-free in Framed due to lack of pinning [0m[0m[1m[31mDate:         [0m 2020-01-30 [0m[0m[1m[31mID:           [0m RUSTSEC-2020-0049 [0m[0m[1m[31mURL:          [0m https://rustsec.org/advisories/RUSTSEC-2020-0049 [0m[0m[1m[31mSolution:     [0m Upgrade to >=0.3.0-beta.1 [0m[0m[1m[31mDependency tree:[0m actix-codec 0.4.0-beta.1 ├── awc 3.0.0-beta.3 │   └── actix-web 4.0.0-beta.4 │       └── zero2prod 0.1.0 ├── actix-web 4.0.0-beta.4 ├── actix-utils 3.0.0-beta.2 │   ├── actix-web 4.0.0-beta.4 │   ├── actix-tls 3.0.0-beta.4 │   │   └── actix-http 3.0.0-beta.4 │   │       ├── awc 3.0.0-beta.3 │   │       └── actix-web 4.0.0-beta.4 │   ├── actix-server 2.0.0-beta.3 │   │   └── actix-web 4.0.0-beta.4 │   └── actix-http 3.0.0-beta.4 ├── actix-tls 3.0.0-beta.4 ├── actix-server 2.0.0-beta.3 └── actix-http 3.0.0-beta.4
    [0m[0m[1m[31mCrate:        [0m actix-http
    [0m[0m[1m[31mVersion:      [0m 3.0.0-beta.4
    [0m[0m[1m[31mTitle:        [0m Use-after-free in BodyStream due to lack of pinning
    [0m[0m[1m[31mDate:         [0m 2020-01-24
    [0m[0m[1m[31mID:           [0m RUSTSEC-2020-0048
    [0m[0m[1m[31mURL:          [0m https://rustsec.org/advisories/RUSTSEC-2020-0048
    [0m[0m[1m[31mSolution:     [0m Upgrade to >=2.0.0-alpha.1
    [0m[0m[1m[31mDependency tree:[0m
    actix-http 3.0.0-beta.4
    ├── awc 3.0.0-beta.3
    │   └── actix-web 4.0.0-beta.4
    │       └── zero2prod 0.1.0
    └── actix-web 4.0.0-beta.4
    
    [0m[0m[1m[31mCrate:        [0m actix-web
    [0m[0m[1m[31mVersion:      [0m 4.0.0-beta.4
    [0m[0m[1m[31mTitle:        [0m Multiple memory safety issues
    [0m[0m[1m[31mDate:         [0m 2018-06-08
    [0m[0m[1m[31mID:           [0m RUSTSEC-2018-0019
    [0m[0m[1m[31mURL:          [0m https://rustsec.org/advisories/RUSTSEC-2018-0019
    [0m[0m[1m[31mSolution:     [0m Upgrade to >=0.7.15
    [0m[0m[1m[31mDependency tree:[0m
    actix-web 4.0.0-beta.4
    └── zero2prod 0.1.0
    
    [0m[0m[1m[33mCrate:        [0m stdweb
    [0m[0m[1m[33mVersion:      [0m 0.4.20
    [0m[0m[1m[33mWarning:      [0m unmaintained
    [0m[0m[1m[33mTitle:        [0m stdweb is unmaintained
    [0m[0m[1m[33mDate:         [0m 2020-05-04
    [0m[0m[1m[33mID:           [0m RUSTSEC-2020-0056
    [0m[0m[1m[33mURL:          [0m https://rustsec.org/advisories/RUSTSEC-2020-0056
    [0m[0m[1m[33mDependency tree:[0m
    stdweb 0.4.20
    └── time 0.2.26
        ├── cookie 0.14.4
        │   └── actix-http 3.0.0-beta.4
        │       ├── awc 3.0.0-beta.3
        │       │   └── actix-web 4.0.0-beta.4
        │       │       └── zero2prod 0.1.0
        │       └── actix-web 4.0.0-beta.4
        ├── actix-web 4.0.0-beta.4
        └── actix-http 3.0.0-beta.4
    
    [0m[0m[1m[33mwarning:[0m 1 allowed warning found
    [0m[0m[1m[32m      Fixing[0m vulnerable dependencies in `Cargo.toml`
    [0m[1m[32m    Upgrading [0mactix-web v4.0.0-beta.4 -> v>=0.7.15 broke the app

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,25 @@ async fn health_check(_req: HttpRequest) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health-check", web::get().to(health_check)))
-        // .bind(address)?
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health-check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    // .bind(address)?
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,17 +1,5 @@
 use std::net::TcpListener;
 
-fn spawn_app() -> String {
-    // Port 0 will have the OS assign a random available portnumber
-    // this will ensure that multiple parallel tests do not attempt to bind to the same port
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port.");
-    let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
-
-    let _ = tokio::spawn(server);
-
-    format!("http://127.0.0.1:{}", port)
-}
-
 #[actix_rt::test]
 async fn health_check_works() {
     // Arrange
@@ -28,6 +16,18 @@ async fn health_check_works() {
     // Assert
     assert!(response.status().is_success()); // 200 OK
     assert_eq!(Some(0), response.content_length()); // no body
+}
+
+fn spawn_app() -> String {
+    // Port 0 will have the OS assign a random available portnumber
+    // this will ensure that multiple parallel tests do not attempt to bind to the same port
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port.");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
 }
 
 #[actix_rt::test]
@@ -50,5 +50,34 @@ async fn subscribe_returns_200_for_valid_form_data() {
     assert_eq!(200, response.status().as_u16());
 }
 
-// #[actix_rt::test]
-// async fn subscribe_returns_a_400_when_data_is_missing() {}
+#[actix_rt::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    // Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // Act
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        // Assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            // Additional customised error message on test Failure
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
+}
